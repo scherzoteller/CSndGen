146bae921f776b607d35924c31f8e47e37450ea8
scherzoteller <loret.vincent@gmail.com>
2016-02-07 12:59:28 +0100

    Clean  obsolete Generate arbo

--- Generate/src/org/scherzoteller/csdnGen/Seeder.scala
+++
@@ -1,8 +0,0 @@
-package org.scherzoteller.csdnGen
-
-trait Seeder {
-	def getUnquantizedValue(): BigDecimal;
-	def getQuantizedValue(quantizer: Quantizer): BigDecimal = {
-	  quantizer.quantize(getUnquantizedValue());
-	}
-}
